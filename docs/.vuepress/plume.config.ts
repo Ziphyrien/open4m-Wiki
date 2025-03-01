70495bf1db651e2c6917a7a52590b81a1001bde0
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-01 15:23:08 +0800

    Update plume.config.ts

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -6,16 +6,8 @@ import { notes } from './notes'
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://zip-image.pages.dev/file/AgACAgUAAyEGAASIL8CVAAMEZ76mh55DFETqT1_q02EN5SMEIrsAAhvBMRsz6vlV1XESC5m6xcIBAAMCAANtAAM2BA.png',
 
   appearance: true,  // 配置 深色模式
-
-  social: [
-    { icon: 'github', link: '/' },
-  ],
-  // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
-  // aside: true, // 页内侧边栏， 默认显示在右侧
-  // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
   /**
    * 文章版权信息
